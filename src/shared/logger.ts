5a3ab20785b32214b29aa08019392d044df696d9
Debashish D. Dev <hello@debashish.dev>
2023-12-12 14:24:14 +0600

    Update logger format and add timestamp

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,24 +1,33 @@
-import winston from 'winston'
+import { createLogger, format, transports } from 'winston'
+const { combine, timestamp, label, printf } = format
+// eslint-disable-next-line import/first
 import path from 'path'
 
-export const logger = winston.createLogger({
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp)
+  const hour = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+  return `${date.toDateString()} ${hour}:${minutes}:${seconds} [${label}] ${level}: ${message}`
+})
+export const logger = createLogger({
   level: 'silly',
-  format: winston.format.json(),
+  format: combine(label({ label: '😐' }), timestamp(), myFormat),
   defaultMeta: { service: 'user-service' },
   transports: [
-    new winston.transports.File({
+    new transports.File({
       filename: path.join(process.cwd(), 'logs', 'winston', 'silly.log'),
       level: 'silly'
     })
   ]
 })
 
-export const errorLogger = winston.createLogger({
+export const errorLogger = createLogger({
   level: 'error',
-  format: winston.format.json(),
+  format: combine(label({ label: '❌' }), timestamp(), myFormat),
   defaultMeta: { service: 'user-service' },
   transports: [
-    new winston.transports.File({
+    new transports.File({
       filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
       level: 'error'
     })
